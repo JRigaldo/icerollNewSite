8907682ab043b7601f3af21145c1d089aa74411a
Jérémy Rigaldo <rigaldo.jeremy@gmail.com>
2019-10-16 11:45:31 +0200

    change name of home template part links

--- home.php
+++ home.php
@@ -5,16 +5,16 @@
 
     <?php if(is_home() && !is_paged()) : ?>
 
-    <!-- Ajax admin buttonfor add posts -->
+    <!-- Ajax admin add posts button -->
     <!--?php require get_template_directory() .'inc/templates/add-posts-api.php'; ?-->
 
     <!-- Main article -->
-    <?php require get_template_directory() .'/inc/templates/home-main-article.php'; ?>
+    <?php require get_template_directory() .'/inc/templates/home-display-main-page.php'; ?>
 
     <?php endif; ?>
 
     <!-- Page articles -->
-    <?php require get_template_directory() .'/inc/templates/home-article.php'; ?>
+    <?php require get_template_directory() .'/inc/templates/home-display-pages.php'; ?>
 
     <!-- Blog posts -->
     <?php require get_template_directory() .'/inc/templates/home-blog-posts.php'; ?>
