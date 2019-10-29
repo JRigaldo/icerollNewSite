546438fe321006628b19975f594e377ba1ef7cfb
Jérémy Rigaldo <rigaldo.jeremy@gmail.com>
2019-10-29 23:03:46 +0100

    add cards css html

--- inc/templates/home-pages-displayed.php
+++ inc/templates/home-pages-displayed.php
@@ -1,29 +1,33 @@
 <!-- Posts Page -->
-<section class="posts pages">
-    
+<section class="posts">
     <?php if(have_rows('home_pages_displayed', 'option')) : ?>
 
     <?php while(have_rows('home_pages_displayed', 'option')) : the_row(); ?>
-    <article>
+    <article class="post">
 
-        <header>
-            <h2><?php the_sub_field('hero_pages_title', 'option'); ?></h2>
-        </header>
+        <?php $image = get_sub_field('hero_pages_image', 'option') ?>
+        <?php $link = get_sub_field('hero_pages_link', 'option') ?>
+        <div class="card">
+            <header>
+                <a href="<?php echo $link; ?>" class="image fit"><img src="<?php echo $image['url'];?>" alt=""></a>
+            </header>
 
-    <?php $image = get_sub_field('hero_pages_image', 'option') ?>
-    <?php $link = get_sub_field('hero_pages_link', 'option') ?>
+            <div class="card-container">
+                <h2><?php the_sub_field('hero_pages_title', 'option'); ?></h2>
 
-        <a href="<?php echo $link; ?>" class="image fit"><img src="<?php echo $image['url'];?>" alt=""></a>
+                <p><?php the_sub_field('hero_pages_text', 'option'); ?></p>
 
-        <p><?php the_sub_field('hero_pages_text', 'option'); ?></p>
-
-        <ul class="actions special">
-            <li><a href="<?php echo $link; ?>" class="button"><?php the_sub_field('hero_pages_button', 'option'); ?></a></li>
-        </ul>
+                <ul class="actions special">
+                    <li><a href="<?php echo $link; ?>" class="button"><?php the_sub_field('hero_pages_button', 'option'); ?></a></li>
+                </ul>
+            </div>
+        </div>
+        
     </article>
 
     <?php endwhile; ?>
     <?php else: ?>
     <?php endif; ?>
-   
 </section>
+    
+   
